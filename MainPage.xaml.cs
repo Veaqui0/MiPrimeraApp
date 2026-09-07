ffd0800e0d74430a6432c8240894d5e286b23259
Veaqui0 <165055306+Veaqui0@users.noreply.github.com>
2026-09-06 19:05:30 -0500

    Manejo de MainPage

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,23 +1,20 @@
 ﻿namespace MiPrimeraApp;
-
+ 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object? sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+ 
+    private void OnSaludarClicked(object sender, EventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(nombreEntry.Text))
+        {
+            saludoLabel.Text = "Por favor escribe un nombre";
+            return;
+        }
+ 
+        saludoLabel.Text = $"¡Hola, {nombreEntry.Text}!";
+    }
 }
